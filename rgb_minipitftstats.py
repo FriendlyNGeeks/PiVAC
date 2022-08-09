d6b70fdb3ba3dd08beefbedf7e13b4c9f2fe1e5c
FriendlyNGeeks <32318784+FriendlyNGeeks@users.noreply.github.com>
2022-08-08 23:22:02 -0400

    Update rgb_minipitftstats.py

--- rgb_minipitftstats.py
+++ rgb_minipitftstats.py
@@ -15,39 +15,6 @@ from PIL import Image, ImageDraw, ImageFont
 from adafruit_rgb_display import st7789
 import fetchTemps
 
-# START WEB SERVER
-#####################################################################
-# Python 3 server example
-from http.server import BaseHTTPRequestHandler, HTTPServer
-
-hostName = "localhost"
-serverPort = 80
-
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        # self.send_header("Content-type", "text/html")
-        # self.end_headers()
-        # self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
-        # self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        # self.wfile.write(bytes("<body>", "utf-8"))
-        # self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        # self.wfile.write(bytes("</body></html>", "utf-8"))
-
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
-
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
-
-    webServer.server_close()
-    print("Server stopped.")
-#####################################################################
-# END WEB SERVER
-
 shutdown_eval = False
 
 # Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
