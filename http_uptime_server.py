8538164021ba698cbc7c2ba0d89a8073b840b4c8
FriendlyNGeeks <32318784+FriendlyNGeeks@users.noreply.github.com>
2022-08-08 23:35:42 -0400

    Update http_uptime_server.py

--- http_uptime_server.py
+++ http_uptime_server.py
@@ -10,8 +10,8 @@ serverPort = 80
 class MyServer(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        # self.send_header("Content-type", "text/html")
-        # self.end_headers()
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
         # self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
         # self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
         # self.wfile.write(bytes("<body>", "utf-8"))
