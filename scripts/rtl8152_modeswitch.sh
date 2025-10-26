d77a940e88d1ff1a8ce3fac03c233eb51b7a2b2f
FriendlyNGeeks <32318784+FriendlyNGeeks@users.noreply.github.com>
2025-10-25 20:03:29 -0400

    Replace wlan0 down command with nmcli radio wifi off

--- scripts/rtl8152_modeswitch.sh
+++ scripts/rtl8152_modeswitch.sh
@@ -3,6 +3,7 @@
 # Run usb_modeswitch command to put the adapter in NIC mode.
 # Source: https://forum.manjaro.org/t/sabrent-usb-2-5g-ethernet-adapter-realtek-8152-chipset-drivers-from-aur/55483
 # https://forums.raspberrypi.com/viewtopic.php?t=334527
+# DEPRECEATED | sh -c "ip link set wlan0 down"
 
-sh -c "ip link set wlan0 down"
+sh -c "nmcli radio wifi off"
 sh -c "usb_modeswitch -v 0bda -p 8151 -M 555342430860d9a9c0000000800006e0000000000000000000000000000000"
